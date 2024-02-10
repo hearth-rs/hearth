a70223805e1acdb7d66f78c077ce9045254d59bd
marceline-cramer <cramermarceline@gmail.com>
2024-02-10 13:46:08 -0700

    schema: add full materials support to protocol

--- core/schema/src/renderer.rs
+++ core/schema/src/renderer.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Hearth. If not, see <https://www.gnu.org/licenses/>.
 
-use glam::{Mat4, UVec2, Vec2, Vec3, Vec4};
+use glam::{Mat3, Mat4, UVec2, Vec2, Vec3, Vec4};
 use serde::{Deserialize, Serialize};
 use serde_with::{base64::Base64, serde_as};
 
@@ -113,8 +113,158 @@ pub enum ObjectUpdate {
 /// A material lump's data format.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct MaterialData {
-    /// The lump ID of the [TextureData] to use for the material's albedo.
-    pub albedo: LumpId,
+    pub albedo: AlbedoComponent,
+    pub transparency: Transparency,
+    pub normal: Option<NormalTexture>,
+    pub aomr_textures: AoMRTextures,
+    pub ao_factor: Option<f32>,
+    pub metallic_factor: Option<f32>,
+    pub roughness_factor: Option<f32>,
+    pub clearcoat_textures: ClearcoatTextures,
+    pub clearcoat_factor: Option<f32>,
+    pub clearcoat_roughness_factor: Option<f32>,
+    pub emissive: MaterialComponent<Vec3>,
+    pub reflectance: MaterialComponent<f32>,
+    pub anisotropy: MaterialComponent<f32>,
+    pub uv_transform0: Mat3,
+    pub uv_transform1: Mat3,
+    pub unlit: bool,
+    pub sample_type: SampleType,
+}
+
+/// How a material's albedo color should be determined.
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AlbedoComponent {
+    /// Albedo color factors in vertex value.
+    ///
+    /// Inner value enables conversion from srgb -> linear before multiplication.
+    pub vertex: Option<bool>,
+
+    /// Albedo factors in a fixed value.
+    pub value: Option<Vec4>,
+
+    /// Albedo factor is sampled from a given [TextureData] lump.
+    pub texture: Option<LumpId>,
+}
+
+/// How transparency should be handled in a material.
+#[derive(Debug, Copy, Clone, Deserialize, Serialize)]
+pub enum Transparency {
+    /// Alpha is completely ignored.
+    Opaque,
+
+    /// Pixels with alpha less than `cutout` is discarded.
+    Cutout { cutout: f32 },
+
+    /// Alpha is blended.
+    Blend,
+}
+
+/// How a material's normals should be derived.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct NormalTexture {
+    /// The [TextureData] of this normal texture.
+    pub texture: LumpId,
+
+    /// The direction of the texture's normals.
+    pub direction: NormalTextureYDirection,
+
+    /// The texture's components to use for normal mapping.
+    pub components: NormalTextureComponents,
+}
+
+/// The direction of the Y (i.e. green) value in the normal maps.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum NormalTextureYDirection {
+    /// Right handed. X right, Y up. OpenGL convention.
+    Up,
+
+    /// Left handed. X right, Y down. DirectX convention.
+    Down,
+}
+
+/// A normal map's component configuration.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum NormalTextureComponents {
+    /// Normal stored in RGB values.
+    Tricomponent,
+
+    /// Normal stored in RG values, third value is reconstructed.
+    Bicomponent,
+
+    /// Normal stored in green and alpha values, third value is reconstructed.
+    ///
+    /// Useful for storing in BC3 or BC7 compressed textures.
+    BicomponentSwizzled,
+}
+
+/// How the Ambient Occlusion, Metalic, and Roughness values should be
+/// determined.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum AoMRTextures {
+    None,
+    Combined {
+        /// Texture with Ambient Occlusion in R, Roughness in G, and Metallic in
+        /// B
+        texture: Option<LumpId>,
+    },
+    SwizzledSplit {
+        /// Texture with Ambient Occlusion in R
+        ao_texture: Option<LumpId>,
+        /// Texture with Roughness in G and Metallic in B
+        mr_texture: Option<LumpId>,
+    },
+    Split {
+        /// Texture with Ambient Occlusion in R
+        ao_texture: Option<LumpId>,
+        /// Texture with Roughness in R and Metallic in G
+        mr_texture: Option<LumpId>,
+    },
+    BWSplit {
+        /// Texture with Ambient Occlusion in R
+        ao_texture: Option<LumpId>,
+        /// Texture with Metallic in R
+        m_texture: Option<LumpId>,
+        /// Texture with Roughness in R
+        r_texture: Option<LumpId>,
+    },
+}
+
+/// How material clearcoat values should be derived.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum ClearcoatTextures {
+    None,
+    GltfCombined {
+        /// Texture with Clearcoat in R, and Clearcoat Roughness in G
+        texture: Option<LumpId>,
+    },
+    GltfSplit {
+        /// Texture with Clearcoat in R
+        clearcoat_texture: Option<LumpId>,
+        /// Texture with Clearcoat Roughness in G
+        clearcoat_roughness_texture: Option<LumpId>,
+    },
+    BWSplit {
+        /// Texture with Clearcoat in R
+        clearcoat_texture: Option<LumpId>,
+        /// Texture with Clearcoat Roughness in R
+        clearcoat_roughness_texture: Option<LumpId>,
+    },
+}
+
+/// Generic container for a component of a material that could either be from a
+/// texture or a fixed value.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct MaterialComponent<T> {
+    pub value: Option<T>,
+    pub texture: Option<LumpId>,
+}
+
+/// How textures should be sampled.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum SampleType {
+    Nearest,
+    Linear,
 }
 
 /// A mesh lump's data format.
